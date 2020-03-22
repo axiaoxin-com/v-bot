0277eb19e804d4dfe822a25da21ef9d132f684b5
axiaoxin <254606826@qq.com>
2020-03-22 17:41:07 +0800

    Revert "remove config.toml in tar"
    This reverts commit b04ee0d886e1a08228f5991fff396600e534d2f8.

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 rm cuitclock.tar.gz
 statik -src=./assets
 env GOOS=linux go build -x -o cuitclock
-tar czvf cuitclock.tar.gz cuitclock
+tar czvf cuitclock.tar.gz cuitclock config.toml
 rm cuitclock
