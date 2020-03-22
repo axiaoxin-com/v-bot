b04ee0d886e1a08228f5991fff396600e534d2f8
axiaoxin <254606826@qq.com>
2020-03-22 16:21:45 +0800

    remove config.toml in tar

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 rm cuitclock.tar.gz
 statik -src=./assets
 env GOOS=linux go build -x -o cuitclock
-tar czvf cuitclock.tar.gz cuitclock config.toml
+tar czvf cuitclock.tar.gz cuitclock
 rm cuitclock
