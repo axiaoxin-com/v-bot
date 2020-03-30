a7fb6a407fd3c8f0664f0f8e95b57788b0f7eba8
axiaoxin <254606826@qq.com>
2020-03-31 02:09:30 +0800

    level use const

--- cnarea/cnarea.go
+++ cnarea/cnarea.go
@@ -6,8 +6,8 @@ package cnarea
 
 import (
 	"fmt"
-	"log"
 
+	// use mysql db
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 )
@@ -31,7 +31,6 @@ const (
 // NewDB 创建sqlx DB实例
 func NewDB(host string, port int, user, passwd string) (*sqlx.DB, error) {
 	mydsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/cnarea?charset=utf8&parseTime=true", user, passwd, host, port)
-	log.Println("[DEBUG] cnarea mysql dsn:", mydsn)
 	db, err := sqlx.Open("mysql", mydsn)
 	if err != nil {
 		return nil, err
@@ -42,7 +41,7 @@ func NewDB(host string, port int, user, passwd string) (*sqlx.DB, error) {
 // Province 查询指定省级、直辖市级区域、特别行政区级信息
 func Province(db *sqlx.DB, shortName string) (Area, error) {
 	area := Area{}
-	if err := db.Get(&area, "select * from cnarea_2018 where level = 0 and short_name = ?", shortName); err != nil {
+	if err := db.Get(&area, "select * from cnarea_2018 where level = ? and short_name = ?", ProvinceLevel, shortName); err != nil {
 		return area, err
 	}
 	return area, nil
@@ -51,7 +50,7 @@ func Province(db *sqlx.DB, shortName string) (Area, error) {
 // Provinces 查询指定省级、直辖市级区域、特别行政区级列表
 func Provinces(db *sqlx.DB) ([]Area, error) {
 	areas := []Area{}
-	if err := db.Select(&areas, "select * from cnarea_2018 where level = 0"); err != nil {
+	if err := db.Select(&areas, "select * from cnarea_2018 where level = ?", ProvinceLevel); err != nil {
 		return nil, err
 	}
 	return areas, nil
@@ -60,7 +59,7 @@ func Provinces(db *sqlx.DB) ([]Area, error) {
 // City 查询指定市级、直辖区级信息
 func City(db *sqlx.DB, shortName string) (Area, error) {
 	area := Area{}
-	if err := db.Get(&area, "select * from cnarea_2018 where level = 1 and short_name = ?", shortName); err != nil {
+	if err := db.Get(&area, "select * from cnarea_2018 where level = ? and short_name = ?", CityLevel, shortName); err != nil {
 		return area, err
 	}
 	return area, nil
@@ -73,7 +72,7 @@ func Cities(db *sqlx.DB, proviceShortName string) ([]Area, error) {
 	if err != nil {
 		return nil, err
 	}
-	if err := db.Select(&areas, "select * from cnarea_2018 where level = 1 and parent_code = ?", provinceArea.AreaCode); err != nil {
+	if err := db.Select(&areas, "select * from cnarea_2018 where level = ? and parent_code = ?", CityLevel, provinceArea.AreaCode); err != nil {
 		return nil, err
 	}
 	return areas, nil
@@ -86,7 +85,7 @@ func Districts(db *sqlx.DB, cityShortName string) ([]Area, error) {
 	if err != nil {
 		return nil, err
 	}
-	if err := db.Select(&areas, "select * from cnarea_2018 where level = 2 and parent_code = ?", cityArea.AreaCode); err != nil {
+	if err := db.Select(&areas, "select * from cnarea_2018 where level = ? and parent_code = ?", DistrictLevel, cityArea.AreaCode); err != nil {
 		return nil, err
 	}
 	return areas, nil
