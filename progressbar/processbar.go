85b7a10ede091e1e00aebf5462c7b2389c7a5708
axiaoxin <254606826@qq.com>
2020-04-03 00:15:28 +0800

    rand theme fix

--- progressbar/processbar.go
+++ progressbar/processbar.go
@@ -38,7 +38,12 @@ func ProgressBar(theme progressbar.Theme, width, total, current int) string {
 // DayProgressBar 今日使用进度
 func DayProgressBar(t time.Time) string {
 	// 使用day作为seed，一天内使用相同主题
-	rand.Seed(int64(t.Day()))
+	day := t.Day()
+	// 0点进度条撑满，需要回退一天保持之前的进度
+	if t.Hour() == 0 {
+		day--
+	}
+	rand.Seed(int64(day))
 	saucerAndPadding := saucerAndPaddings[rand.Intn(len(saucerAndPaddings))]
 	theme := progressbar.Theme{Saucer: saucerAndPadding[0], SaucerHead: "", SaucerPadding: saucerAndPadding[1], BarStart: "", BarEnd: ""}
 
@@ -59,7 +64,12 @@ func isLeap(year int) bool {
 // YearProgressBar 今年使用进度
 func YearProgressBar(t time.Time) string {
 	// 使用year作为seed，一年内使用相同主题
-	rand.Seed(int64(t.Year()))
+	// 一年最后一天凌晨0点撑满进度条，回退一年
+	year := t.Year()
+	if int(t.Month()) == 1 && t.Day() == 1 && t.Hour() == 0 {
+		year--
+	}
+	rand.Seed(int64(year))
 	saucerAndPadding := saucerAndPaddings[rand.Intn(len(saucerAndPaddings))]
 	theme := progressbar.Theme{Saucer: saucerAndPadding[0], SaucerHead: "", SaucerPadding: saucerAndPadding[1], BarStart: "", BarEnd: ""}
 
